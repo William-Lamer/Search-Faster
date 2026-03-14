94900832fb19d42f4f4d71d9d132f1593c87121c
William <william@Williams-MacBook-Pro.local>
2026-03-14 18:42:15 -0400

    added options for opening in a new tab

--- background.js
+++ background.js
@@ -1,10 +1,13 @@
 chrome.commands.onCommand.addListener((command) => {
     console.log(`Command: ${command}`);
-    if (command === "open_first_result") {
-        chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-            chrome.tabs.sendMessage(tabs[0].id, {action: "openFirst"});
-        });
-    }
-});
+    //if (command === "open_first_result") {
+    //    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+    //        chrome.tabs.sendMessage(tabs[0].id, {action: "openFirst"});
+    //   });
+    //}
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+        chrome.tabs.sendMessage(tabs[0].id, {action: command });
+    });
 
+});
 
--- content.js
+++ content.js
@@ -1,8 +1,18 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === "openFirst"){
+    if (request.action === "open_first_result"){
         const firstResult = document.querySelector(".zReHs");
         if (firstResult){
             firstResult.click();
         }
     }
+
+    if (request.action === "open_first_result_new_tab"){
+        const firstResult = document.querySelector(".zReHs");
+        if (firstResult){
+            window.open(firstResult.href, "_blank");
+        }
+    }
+
+
+
 });
